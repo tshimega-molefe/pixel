21ac510fcec5245730f27a9ebb1781f5db956c8b
tshimegabelmont <tshimegabelmont@gmail.com>
2024-05-20 17:56:29 +0200

    created Google Authentication View in Pixel_Auth App

--- backend/pixel_backend/pixel_auth/views.py
+++ backend/pixel_backend/pixel_auth/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from dj_rest_auth.registration.views import SocialLoginView
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from django.conf import settings
+
+
+class GoogleLoginView(SocialLoginView):
+    authentication_classes = (
+        []
+    )  # Disable authentication, make sure to override 'allowed origins' in settings.py in production
+    adapter_class = GoogleOAuth2Adapter
+    callback_url = "http://localhost:3000"
+    client_class = OAuth2Client
